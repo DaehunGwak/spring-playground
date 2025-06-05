ca848280b8b2c8ec1d8838736cdd515fc0c9c888
DaehunGwak <rhkrgns56@naver.com>
2025-06-05 23:49:30 +0900

    [clean] feat: init project

---
+++ clean-architecture/src/main/kotlin/io/ordi/cleanarchitecture/CleanArchitectureApplication.kt
@@ -0,0 +1,11 @@
+package io.ordi.cleanarchitecture
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CleanArchitectureApplication
+
+fun main(args: Array<String>) {
+    runApplication<CleanArchitectureApplication>(*args)
+}
---
+++ clean-architecture/src/test/kotlin/io/ordi/cleanarchitecture/CleanArchitectureApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.ordi.cleanarchitecture
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CleanArchitectureApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
